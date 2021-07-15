0702742a35d71910d6041e74e5cdca77cb44eb19
jahnavi2706 <85930861+jahnavi2706@users.noreply.github.com>
2021-07-15 23:09:00 +0530

    Add files via upload

---
+++ data size.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{ 
+  int integer;
+  float inputfloat;
+  char character;
+  
+  printf("int is    %2d bytes \n", sizeof(short int));
+  printf("float is    %2d bytes \n", sizeof(float));
+  printf("char is   %2d bytes \n", sizeof(char));
+  return 0;
+
+} 
+

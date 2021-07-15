c2c439877c22a8d110acd0157b724024758cf128
jahnavi2706 <85930861+jahnavi2706@users.noreply.github.com>
2021-07-15 23:06:39 +0530

    Update simple interest.cpp

--- simple interest.cpp
+++ simple interest.cpp
@@ -1,10 +1,10 @@
 #include<stdio.h>
 int main()
 {
-int p=20,r=4,t=30;
+int a=20,b=4,c=30;
 float interest;
 
-interest=((p*r*t)/100);
+interest=((a*b*c)/100);
 printf("Simple interest = %d",interest);
 
 return 0;

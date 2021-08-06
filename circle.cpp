26d28d6dd0d257faa3ebbdd063e9e726bfb11c03
jahnavi2706 <85930861+jahnavi2706@users.noreply.github.com>
2021-08-06 11:42:36 +0530

    Update circle.cpp

--- circle.cpp
+++ circle.cpp
@@ -5,10 +5,10 @@ int main()
    radius=20;
    
    area=3.14*radius*radius;
-   printf("area of the circle = %f square cm\n",area);
+   printf("area of the circle = %d square cm\n",area);
    
    perimeter=2*3.14*radius;
-   printf("preimeter of the circle = %f cm\n",perimeter);
+   printf("preimeter of the circle = %d cm\n",perimeter);
    
 return(0);
 }
